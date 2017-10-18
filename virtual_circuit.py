9279888899042934e23685bcf72a569ec545d529
Alex Nguyen <alex-nguyen@live.com.au>
2017-10-19 00:32:50 +1100

    delete old named files

--- virtual_circuit.py
+++
@@ -1,7 +0,0 @@
-class VirtualCircuit:
-    def __init__(self, packet_rate=None):
-        # TODO fill out
-        # track: circuitEdges (dictionary), duration, [packet_rate] (if type = circuit)
-        # duration, isActive
-        # getPath (call algorithm)
-        pass
