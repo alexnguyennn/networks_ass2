983533c28d68245c46073d3c59ca3ecf9e7d721b
brianlam38 <brian.lam38@gmail.com>
2017-10-21 21:35:44 +1100

    adding statistics mngr methods

--- statistics_manager.py
+++ statistics_manager.py
@@ -1,8 +1,88 @@
+#!/usr/bin/python3
+
+# REQUIRED STATISTICS FORMAT:
+# 	total number of virtual circuit requests: 200
+# 	total number of packets: 4589
+# 	number of successfully routed packets: 2000
+# 	percentage of successfully routed packets: 43.58
+# 	number of blocked packets: 2589
+# 	percentage of blocked packets: 56.42
+# 	average number of hops per circuit: 5.42
+# 	average cumulative propagation delay per circuit: 120.54
+
+# CHECKLIST FOR PLACING STATS UPDATES:
+#	total_requests   = NO -> place where VC requests are initialised
+#	total_pkts	     = NO -> place where a packets are sent (for each request)
+#	pkt_success_num  = NO ->
+#	pkt_success_rate = N/A
+#	pkt_blocked_num  = NO
+#	pkt_blocked_rate = N/A
+#	circuit_success  = NO
+#	total_hops		 = NO
+#	total_delay		 = NO
+#	ave_hops		 = N/A
+#	ave_delay		 = N/A
+
 class StatisticsManager:
-    def __init__(self):
-        # TODO fill out
-        # calcaverages
-        # counthops(circuit_edges)
-        # updateStats(key, increment/value)
-        # manage num packets via routingperf's  packet rate
-        pass
+	def __init__(self):
+		self.total_requests = 0		    # total VC requests
+		self.total_pkts = 0				# total pkts in VC = pkts per sec * request duration
+
+		self.pkt_success_num = 0		# success pkts = total - blocked
+		self.pkt_success_rate = 0		# success rate = total - blocked / total
+
+		self.pkt_blocked_num = 0		# blocked pkts
+		self.pkt_blocked_rate = 0		# blocked rate = blocked / total
+
+		self.circuit_success = 0		# success circuits
+		self.total_hops = 0				# cumulative hops (successful)
+		self.total_delay = 0			# cumulative prop delay (successful)
+		self.ave_hops = 0				# average hops = total hops / success circuits
+		self.ave_delay = 0				# average delay = total prop delay / success circuits
+
+		# TODO fill out
+		# calcaverages
+		# counthops(circuit_edges)
+		# manage num packets via routingperf's  packet rate
+
+	# main stats update method
+	def update_stats(self, key, increment):
+		# update VC request
+		if key == "request":
+			self.total_requests += increment
+		# update total packets
+		if key == "packets":
+			self.total_pkts += increment
+		# update success and rate
+		if key == "pkt_success":
+			self.pkt_success_num += increment
+			self.pkt_success_rate = self.pkt_success_num / self.total_pkts
+		# update blocked and rate
+		if key == "pkt_blocked":
+			self.pkt_blocked_num += increment
+			self.pkt_blocked_rate = self.pkt_blocked_num / self.total_pkts
+		# update circuit success
+		if key == "circuit_success":
+			self.circuit_success += increment
+		# update hops + average hops
+		if key == "hop":
+			self.total_hops += increment
+			self.ave_hops = self.total_hops / self.circuit_success
+		# update total propagation delay
+		if key == "delay":
+			self.total_delay += increment
+			self.ave_delay = self.total_delay / self.circuit_success
+
+	# output statistics to terminal
+	def print_statistics(self):
+		print("total number of virtual circuit requests: {}".format(self.total_requests))
+		print("total number of packets: {}".format(self.total_pkts))
+		print("number of successfully routed packets: {}".format(self.pkt_success_num))
+		print("percentage of successfully routed packets: {0:.2f}".format(self.pkt_success_rate))
+		print("number of blocked packets: {}".format(self.pkt_blocked_num))
+		print("percentage of blocked packets: {0:.2f}".format(self.pkt_blocked_rate))
+		print("average number of hops per circuit: {0:.2f}".format(self.ave_hops))
+		print("average cumulative propagation delay per circuit: {0:.2f}".format(self.ave_delay))
+
+
+
